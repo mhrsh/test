2928e2e6e8f930ae53cf5beb357eaf4c4061cdb3
mhrsh <hiroshi@dbms.cs.kumamoto-u.ac.jp>
2015-02-07 03:15:15 +0900

    no message

--- app/Controller/SampleController.php
+++ app/Controller/SampleController.php
@@ -6,18 +6,7 @@ class SampleController extends AppController {
 
   public function index() {
     require_once(dirname(__FILE__). "/common.php");
-    $this->modelClass = false;
-
-    // if ($this->request->data){
-    //   $result = "[result]";
-    //   $result .= "<br>sort: ". $this->request->data['sort'];
-    //   $result .= "<br>category_id: ". $this->request->data['category_id'];
-    //   $result .= "<br>query: " . Sanitize::stripAll($this->request->data['query']);
-    // } else {
-    //   $result = "no data.";
-    // }
-    // $this->set("result", $result);
-        
+    $this->modelClass = false;        
     $sort =  !empty($this->request->data["sort"]) && array_key_exists($this->request->data["sort"], $sortOrder) ? $this->request->data["sort"] : "-score";
     $category_id = ctype_digit($this->request->data["category_id"]) && array_key_exists($this->request->data["category_id"], $categories) ? $this->request->data["category_id"] : 1;
      
